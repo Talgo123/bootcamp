9ddec40f8b3a2ff175358c2e7371ceadc627c2d9
talgat <15talgat@mail.ru>
2023-01-26 19:46:17 +0300

    create folder BIG_O_NATION

---
+++ BIG_O_NATION/zadacha_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// O(n^2)
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n; i++)
+// {
+//     for (int j = 1; j <= n; j++)
+//     {
+//         Console.Write(i * j);
+//         Console.Write("\t");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// Второй способ
+// O(n^2 / 2)
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    System.Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j]);
+        Console.Write(" ");
+    }
+    System.Console.WriteLine();
+}
