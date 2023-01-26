1709f6d5ef5649bf35f009b10461449244978bd3
talgat <15talgat@mail.ru>
2023-01-26 16:00:54 +0300

    create folder

---
+++ Algoritmy_C#/zadacha_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// array - 10
+// сумма 3 подряд идущих элементов m - 3
+//
+// 1 5 4 1 3 4 7 4 1 2
+// m = 3
+// MAX = 15
+
+using System.Diagnostics;
+
+int size = 1_000_000;
+int m = 30000;
+int[] array = Enumerable.Range(1, size).Select(item => Random.Shared.Next(10)).ToArray();
+
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+for (int i = 0; i < array.Length - m; i++)
+{
+    int t = 0;
+    for (int j = i; j < i + m; j++)
+    {
+        t = t + array[j];
+    }
+    if (t > max)
+        max = t;
+}
+
+sw.Stop();
+System.Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+System.Console.WriteLine(max);
---
+++ Algoritmy_C#/zadacha_1_metod_bistree/Program.cs
@@ -0,0 +1,29 @@
+﻿// array - 10
+// сумма 3 подряд идущих элементов m - 3
+//
+// 1 5 4 1 3 4 7 4 1 2
+// m = 3
+// MAX = 15
+
+using System.Diagnostics;
+
+int size = 1_000_000;
+int m = 3;
+int[] array = Enumerable.Range(1, size).Select(item => Random.Shared.Next(10)).ToArray();
+
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+Stopwatch sw = new();
+sw.Start();
+
+int max = 0;
+for (int j = 0; j < m; j++) max += array[j];
+int t = max;
+for (int i = 1; i < array.Length - m; i++)
+{
+    t = t - array[i - 1] + array [i + (m - 1)];
+    if (t > max) max = t;
+}
+
+sw.Stop();
+System.Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+System.Console.WriteLine(max);
