ae7c177b1708595523fa10acccec165f55f4eedf
talgat <15talgat@mail.ru>
2023-01-27 21:50:10 +0300

    Добавили пример реализации быстрой сортировки

---
+++ Bistraya_sortirovka/zadacha_1/infrastructure.cs
@@ -0,0 +1,24 @@
+using static System.Console;
+using static System.String;
+
+public static class Infrastructure
+{
+    public static int[] CreateArray(this int size, int min = 0, int max = 10)
+    {
+        return Enumerable.Range(1, size)
+                .Select(item => Random.Shared.Next(min, max))
+                .ToArray();
+    }
+
+    public static int[] Print(this int[] array, string separator = ",")
+    {
+        string output = Join(separator, array);
+        WriteLine($"[{output}]");
+        return array;
+    }
+
+    public static double Pow(this int a, double b)
+    {
+        return Math.Pow(a, b);
+    }
+}
